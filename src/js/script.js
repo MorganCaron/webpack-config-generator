bb5721b2ea70597a29eb4125e7568742fb5af412
Morgan <13729664+MorganCaron@users.noreply.github.com>
2019-06-20 03:54:39 +0200

    Generate declaration files (#34)
    * Code cleaning
    
    * Removing JS support
    
    * Generate .d.ts files

--- src/js/script.js
+++
@@ -1,1 +0,0 @@
-export const testJs = () => console.log('Javascript')
--- src/ts/App.ts
+++ src/ts/App.ts
@@ -1,7 +1,3 @@
-import { testJs } from '../js/script'
 import loremIpsum from '../txt/loremIpsum.txt'
-//import img from '../img/example.jpg'
 
-testJs()
 console.log(loremIpsum)
-//console.log(img)
