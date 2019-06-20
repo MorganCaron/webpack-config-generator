b57bb063bbbcd06334b8b10fb81307d368e26fec
Morgan Caron <caron.morgan91@gmail.com>
2019-06-20 03:43:21 +0200

    Code cleaning

--- src/ts/App.ts
+++ src/ts/App.ts
@@ -1,7 +1,5 @@
-import { testJs } from '../js/script'
+//import { testJs } from '../js/script'
 import loremIpsum from '../txt/loremIpsum.txt'
-//import img from '../img/example.jpg'
 
 testJs()
 console.log(loremIpsum)
-//console.log(img)
