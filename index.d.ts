7148b32f4c86a1eea86258ebcd732d91bc222fa5
Morgan <caron.morgan91@gmail.com>
2021-06-01 03:25:27 +0200

    Minor fix

--- index.d.ts
+++ index.d.ts
@@ -83,6 +83,16 @@ declare module "*.html" {
 	export default src;
 }
 
+declare module "*.glb" {
+	const src: string;
+	export default src;
+}
+
+declare module "*.gltf" {
+	const src: string;
+	export default src;
+}
+
 declare module "*.module.css" {
 	const classes: { readonly [key: string]: string };
 	export default classes;
